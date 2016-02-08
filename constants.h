74ca97d4a063dc0d91d31171db1e34012d7ccc04
Scott Marshall <scottjmar2@gmail.com>
2016-02-08 13:16:03 -0700

    LEDinit added

---
+++ constants.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   constants.h
+ * Author: Scott
+ *
+ * Created on February 8, 2016, 1:05 PM
+ */
+
+#ifndef CONSTANTS_H
+#define	CONSTANTS_H
+
+#define OUTPUT 0
+#define INPUT 1
+#define ENABLED 1
+#define DISABLED 0
+#define HI_Z 0
+#define LOW_Z 1
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONSTANTS_H */
+
--- leds.c
+++ leds.c
@@ -6,9 +6,17 @@
 */
 
 #include <xc.h>
-
+#include "constants.h"
 
 void initLEDs(){
     //TODO: Initialize the appropriate pins to work with the LEDs
+    TRISGbits.TRISG12 = OUTPUT;     //set LED pins at outputs
+    TRISGbits.TRISG14 = OUTPUT;
+    
+    ODCGbits.ODCG12 = ENABLED;      //Enable Open-Drain Configuration
+    ODCGbits.ODCG14 = ENABLED;
+    
+    LATGbits.LATG12 = HI_Z;         //Turn off the Red LED
+    LATGbits.LATG14 = LOW_Z;        //Turn on the Green LED
     
 }
